03f053bfe707e7e953c7a9b4e015190f8e14b939
nginnever <ginneversource@gmail.com>
2021-10-05 06:53:03 -0700

    init

---
+++ hardhat.config.ts
@@ -0,0 +1,20 @@
+import "@nomiclabs/hardhat-waffle";
+import "@openzeppelin/hardhat-upgrades";
+import "solidity-coverage";
+import "hardhat-deploy";
+//import "@nomiclabs/hardhat-ethers";
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: "0.8.6",
+    allowUnlimitedContractSize: true,
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
